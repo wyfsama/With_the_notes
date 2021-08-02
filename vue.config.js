e3ba6eb6156ed9c149bdf7a21f9e69e0b358f551
wyfsama <6565444+wyfsama@user.noreply.gitee.com>
2021-08-03 00:14:26 +0800

    v 1.0.1 加入title favicon

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,13 @@ module.exports = {
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/With_the_notes/dist'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '随手记 ᶘ ᵒᴥᵒᶅ '
+        return args
+      })
+  }
 }
